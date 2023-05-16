caffc99abb57457c56d11760eead5231df8d5656
chinaluccie <lucianne451@students.uonbi.ac.ke>
2023-05-16 18:29:51 +0000

    debug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+cript that does replace a file path
+
+$file_path = '/var/www/html/wp-settings.php'
+
+#replace "phpp" with "php"
+
+exec { 'ficinge_line':
+
+  command => "sed -i 's/phpp/php/g' ${file_path}",
+
+  path    => ['/bin','/usr/bin']
+
+}
